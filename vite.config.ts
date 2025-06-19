e915384e4bad8237d6f3c1fb76e61aaaae4a40be
FunctionDJ <34871211+FunctionDJ@users.noreply.github.com>
2025-06-19 19:57:27 +0200

    set vite base to dynamic REPO value

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "minze-box-web-editor",
+  base: "/<REPO>/",
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
