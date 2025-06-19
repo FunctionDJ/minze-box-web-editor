e01b23eaf088bfd067d9a8496dd2048de78ed067
FunctionDJ <34871211+FunctionDJ@users.noreply.github.com>
2025-06-19 20:29:34 +0200

    revert REPO variable because it didnt work

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/<REPO>/",
+  base: "minze-box-web-editor",
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
