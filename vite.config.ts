d86d7bc59cbd15345a021e8f3559ca4d09dfe380
FunctionDJ <34871211+FunctionDJ@users.noreply.github.com>
2025-06-19 19:56:54 +0200

    set vite base according to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "minze-box-web-editor",
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
